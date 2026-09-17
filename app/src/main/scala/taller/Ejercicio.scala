d1d27aab3a1900035c9360431bcc23db9967207d
light <juan.jose.quevedo@correounivalle.edu.co>
2026-09-17 16:35:47 -0500

    implementacion de complemento funciones

--- app/src/main/scala/taller/Ejercicio.scala
+++ app/src/main/scala/taller/Ejercicio.scala
@@ -9,12 +9,12 @@ class Ejercicio() {
   @tailrec
   final def sumOfSquares(n: Int, acc: Int = 0): Int = {
     if (n <= 0) acc
-    else sumOfSquares(n - 1, acc) // Completar: falta sumar el cuadrado de n
+    else sumOfSquares(n - 1, acc + n*n) // Completar: falta sumar el cuadrado de n
   }
 
   @tailrec
   final def sumOfNumbers(n: Int, acc: Int = 0): Int = {
     if (n <= 0) acc
-    else sumOfNumbers(n - 1, acc) // Completar: falta sumar n cuando es par
+    else sumOfNumbers(n - 1, acc + (if (n % 2 == 0) n else 0) ) // Completar: falta sumar n cuando es par
   }
 }
